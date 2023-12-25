e0d93b2602fdcac9ae615a18368919902f331bd8
demasarvin11 <demasarvin11@gmail.com>
2023-12-25 14:32:03 +0700

    Add: cursor pointer button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 function Button({ name }) {
   return (
-    <div className="rounded-md bg-gradient-to-b from-violet-500 to-violet-800 px-8 py-2 font-primary">
+    <div className="rounded-md bg-gradient-to-b from-violet-500 to-violet-800 px-8 py-2 font-primary cursor-pointer">
       {name}
     </div>
   );
